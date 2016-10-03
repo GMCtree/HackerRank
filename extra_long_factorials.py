eada5d05ab8159f9e5ef9d6e4b2f1c09d36f1e04
Quinn Perfetto <thequinnftw@gmail.com>
2016-10-03 14:49:51 -0400

    added unit tests

--- extra_long_factorials.py
+++ extra_long_factorials.py
@@ -1,5 +1,11 @@
 def fact(n):
+    if n == 0:
+        return 1
     return reduce(lambda x, y: x * y, xrange(1, n + 1))
+
+assert(fact(5) == 5 * 4 * 3 * 2 * 1)
+assert(fact(0) == 1)
+assert(fact(2) == 2)
 
 num = input()
 print(fact(num))
